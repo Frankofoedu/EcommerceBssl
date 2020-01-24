6be56e8aa4bd902acb5c02a4ac2210c17b081faa
Obinna Okpara <obainoforgod@gmail.com>
2020-01-24 15:53:26 +0100

    Added migrations

--- EcommerceBssl/Data/Migrations/20200124132020_initialTables.Designer.cs
+++ EcommerceBssl/Data/Migrations/20200124132020_initialTables.Designer.cs
@@ -4,14 +4,16 @@ using EcommerceBssl.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EcommerceBssl.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200124132020_initialTables")]
+    partial class initialTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -165,8 +167,6 @@ namespace EcommerceBssl.Data.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<string>("BannerImage");
 
                     b.Property<int>("MainCategoryId");
 
---
+++ EcommerceBssl/Data/Migrations/20200124133827_addedBannerImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceBssl.Data.Migrations
+{
+    public partial class addedBannerImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BannerImage",
+                table: "SubCategories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BannerImage",
+                table: "SubCategories");
+        }
+    }
+}
