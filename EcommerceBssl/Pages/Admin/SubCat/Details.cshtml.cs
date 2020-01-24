dc23b61a6f05416545121a3eb4421db0490996c5
Obinna Okpara <obainoforgod@gmail.com>
2020-01-24 15:55:19 +0100

    CRUD for subcategories

---
+++ EcommerceBssl/Pages/Admin/SubCat/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EcommerceBssl.Data;
+
+namespace EcommerceBssl.Pages.Admin.SubCat
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly EcommerceBssl.Data.ApplicationDbContext _context;
+
+        public DetailsModel(EcommerceBssl.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public SubCategory SubCategory { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            SubCategory = await _context.SubCategories
+                .Include(s => s.MainCategory).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (SubCategory == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ EcommerceBssl/Pages/Admin/SubCat/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EcommerceBssl.Data;
+
+namespace EcommerceBssl.Pages.Admin.SubCat
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EcommerceBssl.Data.ApplicationDbContext _context;
+
+        public IndexModel(EcommerceBssl.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<SubCategory> SubCategory { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            SubCategory = await _context.SubCategories
+                .Include(s => s.MainCategory).ToListAsync();
+        }
+    }
+}
