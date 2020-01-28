d5bfb2632050b083bedd95371727ce9cdd27095c
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-01-28 17:04:34 +0100

    added file upload

---
+++ EcommerceBssl/UtilityMethods/FileUpload.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceBssl.UtilityMethods
+{
+    public static class FileUpload
+    {
+       public static async Task<string> UploadFile(IFormFile formFile, string folderName)
+        {
+            var fileName = DateTime.Now.Ticks.ToString() + formFile.FileName;
+
+            var filepath = Path.Combine(Directory.GetCurrentDirectory(), $@"wwwroot\{folderName}", fileName);
+
+            await formFile.CopyToAsync(new FileStream(filepath, FileMode.Create));
+
+            return $"{folderName}\\{fileName}";
+        }
+    }
+}
